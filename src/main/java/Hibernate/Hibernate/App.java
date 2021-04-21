95adddc8852d8464f783268ddb04db01a2bc28d4
Arturo Hernandez Nuñez <arturohernandez1@usal.es>
2021-04-21 12:09:24 +0200

    Configuracion del logger

--- src/main/java/Hibernate/Hibernate/App.java
+++ src/main/java/Hibernate/Hibernate/App.java
@@ -1,13 +1,23 @@
 package Hibernate.Hibernate;
 
+import org.apache.log4j.Logger;
+
 /**
  * Hello world!
  *
  */
+
+import log.logs.*;
 public class App 
 {
+	
+	
     public static void main( String[] args )
     {
+    	UsoLogger.createLogguer();
+    	Logger log = UsoLogger.getLogger(App.class);
+    	
         System.out.println( "Hello World!" );
+        log.info("Programa iniciado");
     }
 }
